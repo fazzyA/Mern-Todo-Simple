a94fabc8f82d196a0690093c7bc2072b3cc2bf05
fazzyA <faz.pak@gmail.com>
2024-06-30 10:04:27 +0500

    mern todo

---
+++ Backend/Server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes/Todo.js')
+const cors =  require('cors')
+const dbconnect = require('./db.js')
+
+
+require('dotenv').config({ path: '../.env' })
+
+
+const app = express()
+const PORT = process.env.port || 5000
+app.use(express.json())
+app.use(cors())
+dbconnect()
+app.use(routes)
+
+
+app.listen(PORT, () => console.log(`Server is Running on: ${PORT} `))
